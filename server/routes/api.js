1e3137ae4997be58020a6acced8f0ba117fa8bfa
Yosuke Tomita <ytomita615@gmail.com>
2023-06-10 17:51:25 -0700

    work on authenticate user and bcrypt

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,13 +5,15 @@ const userController = require('../controllers/userController');
 const cookieController = require('../controllers/cookieController');
 
 // when user signs up
-router.post('/signup', userController.findUserEmail, userController.createUser, cookieController.setUserCookie, (req, res) => {
+router.post('/signup', userController.findUserEmail, userController.hashPassword, userController.createUser, cookieController.setUserCookie, (req, res) => {
   res.status(200).json({ message: 'User created succesfully' });
 });
 
 //when user logs in
-// router.post('/login', userController.authenticateUser, cookieController.setUserCookie, (req, res) => {
-//   res.status(200).json();
-// });
+router.post('/login', userController.authenticateUser, cookieController.setUserCookie, (req, res) => {
+  res.status(200).json({ message: 'Login successful'});
+});
+
+// when user wants to add expense
 
 module.exports = router;
