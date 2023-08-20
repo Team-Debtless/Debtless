772258a3ea8f8e9fef63441b0df23f9190519a2a
bryan trang <bryanltrang@gmail.com>
2023-08-20 12:50:48 -0700

    trying to access link token

---
+++ client/components/Link.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useContext } from "react";
+import { usePlaidLink } from "react-plaid-link";
+
+// import Context from "../../Context";
+// import {Products} from "plaid"; // TS Types
+
+const Link = () => {
+  const { linkToken, isPaymentInitiation, dispatch } = useContext(Context);
+
+  const onSuccess = React.useCallback(
+    (public_token) => {
+      // If the access_token is needed, send public_token to server
+      const exchangePublicTokenForAccessToken = async () => {
+        const response = await fetch("/api/set_access_token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+          },
+          body: `public_token=${public_token}`,
+        });
+        if (!response.ok) {
+          dispatch({
+            type: "SET_STATE",
+            state: {
+              itemId: `no item_id retrieved`,
+              accessToken: `no access_token retrieved`,
+              isItemAccess: false,
+            },
+          });
+          return;
+        }
+        const data = await response.json();
+        dispatch({
+          type: "SET_STATE",
+          state: {
+            itemId: data.item_id,
+            accessToken: data.access_token,
+            isItemAccess: true,
+          },
+        });
+      };
+
+      // 'payment_initiation' products do not require the public_token to be exchanged for an access_token.
+      if (isPaymentInitiation){
+        dispatch({ type: "SET_STATE", state: { isItemAccess: false } });
+      } else {
+        exchangePublicTokenForAccessToken();
+      }
+
+      dispatch({ type: "SET_STATE", state: { linkSuccess: true } });
+      window.history.pushState("", "", "/");
+    },
+    [dispatch]
+  );
+
+  let isOauth = false;
+  const config = {
+    token: linkToken,
+    onSuccess,
+  };
+
+  if (window.location.href.includes("?oauth_state_id=")) {
+    // TODO: figure out how to delete this ts-ignore
+    // @ts-ignore
+    config.receivedRedirectUri = window.location.href;
+    isOauth = true;
+  }
+
+  const { open, ready } = usePlaidLink(config);
+
+  useEffect(() => {
+    if (isOauth && ready) {
+      open();
+    }
+  }, [ready, open, isOauth]);
+
+  return (
+    <button onClick={() => open()}>
+      Launch Link
+    </button>
+  );
+};
+
+Link.displayName = "Link";
+
+export default Link;
--- client/pages/expenses.js
+++ client/pages/expenses.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Modal from './expense_modal';
+import Link from '../components/Link';
 
 const Expenses = () => {
   return (
     <div className="page">
+      <div>
       <h2 className="title left">Expenses</h2>
+      <Link/>
+      </div>
       <Modal />
     </div>
   );
